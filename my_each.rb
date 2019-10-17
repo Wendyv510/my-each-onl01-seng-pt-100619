3f6bd62bbbedeabffdd43f01f62e217e002438ff
Wendy Harris <wendyv510@hotmail.com>
2019-10-17 18:28:12 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,6 @@
-def my_each # put argument(s) here
-  # code here
+def my_each (words)
+   Array = ["Hi", "Hello", "Bye", "Goodbye"]
+   counter = 0 
+   my_each.collection do |words|
+     counter += 1 
 end
